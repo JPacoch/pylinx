b82b7a92a25edb848162a9bebf86893fab00f16a
JPacoch <pacochp@gmail.com>
2022-11-15 23:20:46 +0100

    algos, but not working v1

--- algo.py
+++ algo.py
@@ -1,19 +1,11 @@
 # algo.py
 
-from time import time
+import time
 
 class Algo():
-    def __init__(self, number, *args, **kwargs):
+    def __init__(self, number, text, *args, **kwargs):
         self.number = number
-
-    @timer
-    def recur_fac(self):
-        if self.number == 1:
-            return self.number
-        else:
-            self.number -= 1
-            result = self.number * self.recur_fac()
-            return result
+        self.text = text
 
     def timer(func):
         def timing(*args, **kwargs):
@@ -25,7 +17,30 @@ class Algo():
 
         return timing
 
+    def recur_fac(self):
+        if self.number == 1:
+            return self.number
+        else:
+            self.number -= 1
+            result = self.number * self.recur_fac()
+            return result
+
+    @timer
+    def permute_string(self, text='', pocket=''):
+        text = self.text
+        if len(self.text) == 0:
+            print('0 lenght string')
+        else:
+            for i in range(len(self.text)):
+                letter = self.text[i]
+                front = self.text[0:i]
+                back = self.text[i+1:]
+                merge = front + back
+                return self.permute_string(merge, letter + '')
 
 if __name__ == '__main__':
-    algo = Algo(6)
-    print(algo.recur_fac())
+    algo = Algo(6, 'Jakub', 4213)
+    print(algo.permute_string())
+
+
+
